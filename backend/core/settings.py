5d641f7fb35f9b08dbfc89f02d9d68bf563713ed
Prem Kumar <thepremkumar04@gmail.com>
2026-07-12 18:42:45 +0530

    Fixed STATIC_ROOT error for Render deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
